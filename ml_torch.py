fdf71bf640af20590328725f980f5b7718653edc
shiertier <90256303+shiertier@users.noreply.github.com>
2023-12-25 21:40:41 +0800

    Create ml_torch.py

---
+++ ml_torch.py
@@ -0,0 +1,25 @@
+from shml.interface import Infer
+model_path = "/gemini/code"
+infer_instance = Infer(model_path)
+image_path = "/gemini/code/1.png"
+from PIL import Image
+image = Image.open(image_path)
+threshold = 0.68
+image_size = 960
+keep_ratio = True
+
+space = True
+escape = True
+conf = True
+
+# Perform inference
+result, result_dict = infer_instance.infer_one(
+    img=image,
+    threshold=threshold,
+    image_size=image_size,
+    keep_ratio=keep_ratio,
+    model_path=model_path,
+    space=space,
+    escape=escape,
+    conf=conf
+)
